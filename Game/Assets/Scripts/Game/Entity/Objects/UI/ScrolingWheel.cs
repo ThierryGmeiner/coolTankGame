aee649a8c2a4de7ffe3a0eacfb22bcb62ded4e30
ThierryGmeiner <122564457+ThierryGmeiner@users.noreply.github.com>
2023-08-02 01:00:51 +0200

    scrolingWheel
    - cleanup code
    - improve performance

--- Game/Assets/Scripts/Game/Entity/Objects/UI/ScrolingWheel.cs
+++ Game/Assets/Scripts/Game/Entity/Objects/UI/ScrolingWheel.cs
@@ -13,9 +13,7 @@ namespace Game.UI
         private ScrolingWheelSegment[] segments;
         private float segmentAngle;
         private int selectedSegmentIndex = 0;
-
-        private readonly Vector3 DEFAULT_SCALE = new Vector3(0.15f, 0.15f, 0.15f);
-        private readonly Vector3 SELECTED_SCALE = new Vector3(0.23f, 0.23f, 0.23f);
+        private int oldSelectedSegmentIndex = 0;
 
         private int segmentCount => items.Length;
 
@@ -39,19 +37,16 @@ namespace Game.UI
         }
 
         private void HighlightSelectedItem() {
-            for (int i = 0; i < segmentCount; i++) {
-                if (i == selectedSegmentIndex) {
-                    segments[i].image.sprite = items[i].SpriteWhenSelected;
-                    segments[i].rect.localScale = SELECTED_SCALE;
-                } else {
-                    segments[i].image.sprite = items[i].Sprite;
-                    segments[i].rect.localScale = DEFAULT_SCALE;
-                }
+            if (selectedSegmentIndex != oldSelectedSegmentIndex) {
+                segments[selectedSegmentIndex].SelectItem();
+                segments[oldSelectedSegmentIndex].DeselectItem();
             }
         }
         
         private int GetSegmentIndex() {
+            oldSelectedSegmentIndex = selectedSegmentIndex;
             float angle = GetAngle();
+
             for (int i = 0; i < segmentCount; i++) {
                 if (angle < ( ( i + 1 ) * segmentAngle ) - ( segmentAngle / 2 ) ) {
                     return i;
@@ -76,32 +71,11 @@ namespace Game.UI
 
             for (int i = 0; i < segmentCount; i++) {
                 float angle = i * segmentAngle;
-                GameObject canvas = InstantiateItemCanvas(items[i].Sprite, angle);
-                canvas.name = "itam_" + i;
-                segments[i] = new(items[i], canvas);                
+                GameObject canvas = ScrolingWheelSegment.InstantiateItemCanvas(transform, angle);
+                segments[i] = new(items[i], canvas);
+                segments[i].DeselectItem();
+                canvas.name = $"item-{i}";
             }
-        }
-
-        private GameObject InstantiateItemCanvas(Sprite sprite, float angle) {
-            // instantiate image and helper
-            GameObject midPoint = new GameObject();
-            GameObject segment = ScrolingWheelSegment.EmptyCanvas();
-            midPoint.transform.SetParent(transform);
-            midPoint.transform.position = transform.position;
-            segment.transform.SetParent(midPoint.transform);
-
-            // mutate item
-            RectTransform rect = segment.GetComponent<RectTransform>();
-            rect.localScale = DEFAULT_SCALE;
-            rect.localPosition = new Vector3(0, 3.5f, 0);
-            midPoint.transform.localRotation = Quaternion.Euler(0, 0, -angle);
-
-            segment.transform.SetParent(transform);
-            rect.localRotation = Quaternion.Euler(0, 0, 0);
-            Destroy(midPoint);
-            segment.GetComponent<Image>().sprite = sprite;
-
-            return segment;
         }
     }
 }
--- Game/Assets/Scripts/Game/Entity/Objects/UI/ScrolingWheelSegment.cs
+++ Game/Assets/Scripts/Game/Entity/Objects/UI/ScrolingWheelSegment.cs
@@ -6,14 +6,48 @@ namespace Game.UI
 {
     public class ScrolingWheelSegment
     {
-        public Item Item { get; }
+        public Item item { get; }
         public Image image { get; }
         public RectTransform rect{ get; }
 
+        private static readonly Vector3 DEFAULT_SCALE = new Vector3(0.15f, 0.15f, 0.15f);
+        private static readonly Vector3 SELECTED_SCALE = new Vector3(0.23f, 0.23f, 0.23f);
+
         public ScrolingWheelSegment(Item item, GameObject obj) {
-            Item = item;
+            this.item = item;
             image = obj.GetComponent<Image>() ?? obj.AddComponent<Image>();
             rect = obj.GetComponent<RectTransform>() ?? obj.AddComponent<RectTransform>();
+        }
+
+        public void SelectItem() {
+            image.sprite = item.SpriteWhenSelected;
+            rect.localScale = SELECTED_SCALE;
+        }
+
+        public void DeselectItem() {
+            image.sprite = item.Sprite;
+            rect.localScale = DEFAULT_SCALE;
+        }
+
+        public static GameObject InstantiateItemCanvas(Transform parent, float angle) {
+            // instantiate image and helper
+            GameObject midPoint = new GameObject();
+            GameObject segment = ScrolingWheelSegment.EmptyCanvas();
+            midPoint.transform.SetParent(parent);
+            midPoint.transform.position = parent.position;
+            segment.transform.SetParent(midPoint.transform);
+
+            // mutate item
+            RectTransform rect = segment.GetComponent<RectTransform>();
+            rect.localScale = DEFAULT_SCALE;
+            rect.localPosition = new Vector3(0, 3.5f, 0);
+            midPoint.transform.localRotation = Quaternion.Euler(0, 0, -angle);
+
+            segment.transform.SetParent(parent);
+            rect.localRotation = Quaternion.Euler(0, 0, 0);
+            GameObject.Destroy(midPoint);
+
+            return segment;
         }
 
         public static GameObject EmptyCanvas() {
